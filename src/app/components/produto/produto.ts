cc407f2bed86de16ead7436beff14baa2f7ce072
Vitoria Luane Malafaia Ferreira <luane2966@gmail>
2026-07-13 21:55:15 -0300

    feat, adicionar a funçao de lista de produto

--- src/app/components/produto/produto.ts
+++ src/app/components/produto/produto.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import{ UpperCasePipe} from '@angular/common';
 
 @Component({
   selector: 'app-produto',
-  imports: [],
+  imports: [UpperCasePipe],
   templateUrl: './produto.html',
   styleUrl: './produto.css',
 })
-export class Produto {}
+// adicionar a classe de Produto ,variavel condicional
+export class Produto {
+  produto = 'Notebook Gamer';
+  preco = 5000;
+  mostrarProduto = true;
+  mostrarPreco = true;
+  produtos =[
+    {nome: 'Teclado', preco: 49.99},
+    {nome: 'Mouse', preco:29.99},
+    {nome: 'Monitor', preco: 149.99},
+  ];
+}
