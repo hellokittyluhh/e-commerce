0474f1b622c433f8832f4bc166ceca4eaab8df0c
Vitoria Luane Malafaia Ferreira <luane2966@gmail>
2026-07-17 19:15:34 -0300

    docs(alterar código do arquivo src/app/app.ts)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 //import { RouterOutlet } from '@angular/router';// remove a importação do RouterOutlet,pois não é necessário para o funcionamento para esse componente
-import {Produto} from './components/produto/produto';// importaqndo a classe de produto do arquivo produto.ts 
+//import {Produto} from './components/produto/produto';// importaqndo a classe de produto do arquivo produto.ts
+import {ListaProdutos} from './features/produtos/lista-produtos/lista-produtos'; 
 @Component({
   selector: 'app-root',
-  imports:[Produto],
+  imports:[ListaProdutos],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl:'./app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
