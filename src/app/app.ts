fcc0b22091e9802062953f4075e617ff7cf2f7fa
Vitoria  Luane Malafaia Ferreira <luane2966@gmail.com>
2026-08-06 21:04:29 -0300

    style(formatação)

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,9 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet,RouterLink } from '@angular/router';// remove a importação do RouterOutlet,pois não é necessário para o funcionamento para esse componente
 import { UpperCasePipe } from '@angular/common';
  import { usuarioLogado,login,logout } from './core/auth';
+ import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { Header } from './shared/layout/header/header';
 @Component({
   selector: 'app-root',
-  imports:[RouterOutlet,RouterLink,UpperCasePipe],
+  imports:[RouterOutlet,RouterLink,UpperCasePipe,MatButtonModule,MatCardModule,Header ],
   templateUrl: './app.html',
   styleUrl:'./app.css'
 })
