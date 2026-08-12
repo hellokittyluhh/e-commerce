16d710f8c34cda8040ada9ca840e556ff2b29154
Vitoria  Luane Malafaia Ferreira <luane2966@gmail.com>
2026-08-11 21:35:52 -0300

    feat (nova funcionalidade)

---
+++ src/app/features/checkout/checkout/checkout.ts
@@ -0,0 +1,44 @@
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
+import { CarrinhoService } from '../../../core/services/carrinho.service';
+import { Validators, AbstractControl, ValidationErrors } from '@angular/forms';
+
+@Component({
+  selector: 'app-checkout',
+  imports: [ReactiveFormsModule],
+  templateUrl: './checkout.html',
+  styleUrl: './checkout.css',
+})
+export class Checkout {
+  
+  carrinhoService = inject(CarrinhoService);
+
+formulario = new FormGroup({
+  nome: new FormControl ('',[Validators.required,Validators.minLength(2), nomeSemNumeros]),
+  email: new FormControl('',[Validators.required,Validators.email]),
+  endereco: new FormControl('',[Validators.required,Validators.minLength(5)]),
+});
+
+finalizar(){
+
+ if (this.formulario.invalid){
+  console.log('Formulario invalido');
+  return;
+ }
+
+const dados = this.formulario.value;
+const itens = this.carrinhoService.itens;
+
+  console.log('Dados do Formulario:' ,this.formulario.value);
+console.log('Itens no Carrinho:',this.carrinhoService.itens());
+ }
+}
+
+ function nomeSemNumeros(controle:AbstractControl):ValidationErrors | null {
+  const valor = controle.value;
+  if(!valor) return null;
+  if(/\d/.test(valor)){
+   return{numeroInvalido:true};
+  }
+  return null;
+  }
