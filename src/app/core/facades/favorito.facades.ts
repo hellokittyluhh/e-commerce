c26e1ea203a6172a2b26767610c21dc4f9158791
Vitória Luane Malafaia Ferreira <luane2966@gmail.com>
2026-09-10 20:34:27 -0300

    feat

--- src/app/core/facades/favorito.facades.ts
+++ src/app/core/facades/favorito.facades.ts
@@ -1,10 +1,1 @@
 import { Injectable, inject } from "@angular/core";
-import {ItemFavorito} from "../models/favorito-item";
-import { favoritosServices } from "../services/favoritos.service";
-import { ProdutoLoja } from "../models/produto-loja";
-@Injectable({providedIn:'root'})
-export class FavoriteFacade {
-
-    private favorService = inject(favoritosServices);
-    itensFavorito = this.favoritoService.
-}
